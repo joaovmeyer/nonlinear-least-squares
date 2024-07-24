47456f1642aa2af7b5d4c78d6ed72f9840728195
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2024-07-24 11:34:13 -0300

    Create Newton.js

---
+++ Newton.js
@@ -0,0 +1,56 @@
+function Newton(X, Y, maxIter = 100)
+
+	N = length(X);
+
+	# normalizing Y value
+	normVal = sum(Y) * 0.1;
+	Y_norm = Y ./ normVal;
+
+	# initialize a and b value
+	positiveYs = [Y_norm[i] >= 1e-5 for i = 1:N];
+	numPositives = sum(positiveYs);
+
+	A = [ones(numPositives) X[positiveYs]];
+
+	l, initialParamB = A \ log.(Y_norm[positiveYs]);
+	initialParamA = exp(l);
+	
+	
+	print("Initial guess for a: ");
+	println(initialParamA * normVal);
+
+	
+	print("Initial guess for b: ");
+	println(initialParamB)
+
+
+	# tunning the initial parameters
+	paramA = initialParamA;
+	paramB = initialParamB;
+
+	for iter = 1:maxIter
+		JTJ = zeros(2, 2);
+		JTr = zeros(2, 1);
+
+		for i = 1:N
+			e = exp(paramB * X[i]);
+			
+			JTJ[1, 1] += e * e;
+			JTJ[1, 2] += paramA * X[i] * e * e;
+			JTJ[2, 1] += paramA * X[i] * e * e;
+			JTJ[2, 2] += paramA * paramA * X[i] * X[i] * e * e;
+
+			ri = paramA * e - Y[i];
+
+			JTr[1, 1] += ri * e;
+			JTr[2, 1] += ri * paramA * X[i] * e;
+		end
+
+		delta = JTJ \ JTr;
+		paramA -= delta[1];
+		paramB -= delta[2];
+		
+	end
+
+	return paramA * normVal, paramB;
+end
