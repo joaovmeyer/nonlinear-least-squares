9180c2ad0fa2fa63f3b5c608a75f8d5e0a2e123d
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2024-07-27 22:33:52 -0300

    Update Newton.jl

--- functions/Newton.jl
+++ functions/Newton.jl
@@ -1,6 +1,17 @@
-function Newton(X, Y, maxIter = 100)
+function Newton(X, Y, maxIter = 100, batchSize = -1)
 
 	N = length(X);
+	println(N);
+
+	# no batching
+	if (batchSize <= 0)
+		batchSize = N;
+	else
+		# shuffle points if using batches
+		ordem = randperm(N)
+		X = X[ordem]
+		Y = Y[ordem]
+	end
 
 	# normalizing Y value
 	normVal = sum(Y) * 0.1;
@@ -31,32 +42,53 @@ function Newton(X, Y, maxIter = 100)
 	paramA = initialParamA;
 	paramB = initialParamB;
 
+	loss = [];
+
+	# points seen since last update
+	pointsSeen = 0;
+
+	# have these outside the training loop because if we are usign batches, the remainder of one batch can be used in the next one.
+	JTJ = zeros(2, 2);
+	JTr = zeros(2, 1);
+
 	for iter = 1:maxIter
-		JTJ = zeros(2, 2);
-		JTr = zeros(2, 1);
+		L = 0.0;
 
 		for i = 1:N
 			e = exp(paramB * X[i]);
 
-			# d(Rᵢ)/da
 			JTJ[1, 1] += e * e;
 			JTJ[1, 2] += paramA * X[i] * e * e;
 			JTJ[2, 1] += paramA * X[i] * e * e;
-			
-			# D(Rᵢ)/db
 			JTJ[2, 2] += paramA * paramA * X[i] * X[i] * e * e;
 
 			ri = paramA * e - Y_norm[i];
 
 			JTr[1, 1] += ri * e;
 			JTr[2, 1] += ri * paramA * X[i] * e;
+			L += ri * ri;
+
+			pointsSeen += 1;
+
+			if (pointsSeen == batchSize)
+				delta = JTJ \ JTr;
+				paramA -= delta[1];
+				paramB -= delta[2];
+			
+				JTJ = zeros(2, 2);
+				JTr = zeros(2, 1);
+				pointsSeen = 0;
+			end
 		end
 
-		delta = JTJ \ JTr;
-		paramA -= delta[1];
-		paramB -= delta[2];
-		
+		push!(loss, L);
 	end
+
+	# maybe at the end some points were seen but not used. It's best to keep
+	# it that way, because if we choose to use them, they have a high chance
+	# of not representing out function well, due to their small number
+
+	
 	print("Final guess for a after $maxIter iterations: ");
 	println(paramA * normVal);
 
@@ -64,6 +96,5 @@ function Newton(X, Y, maxIter = 100)
 	print("Final guess for b after $maxIter iterations: ");
 	println(paramB)
 	
-	return (paramA * normVal, paramB);
+	return paramA * normVal, paramB, loss;
 end
-  
