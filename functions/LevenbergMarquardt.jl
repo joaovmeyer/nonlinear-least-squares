67df885878bc8c4d92cb06353e66babc2c8cf268
joaovmeyer <144701021+joaovmeyer@users.noreply.github.com>
2024-07-28 23:42:22 -0300

    Update LevenbergMarquardt.jl
    batched option, but still need to fix delayed gratification

--- functions/LevenbergMarquardt.jl
+++ functions/LevenbergMarquardt.jl
@@ -1,41 +1,32 @@
-function LevenbergMarquardt(X, Y, maxIter = 100, damping = 5)
+function LevenbergMarquardt(X, Y, maxIter = 100, damping = 5, batchSize = -1)
 
 	N = length(X);
+
+	# no batching
+	if (batchSize <= 0)
+		batchSize = N;
+	else
+		# shuffle points if using batches
+		ordem = randperm(N)
+		X = X[ordem]
+		Y = Y[ordem]
+	end
 
 	# normalizing Y value
 	normVal = sum(Y) * 0.1;
 	Y_norm = Y ./ normVal;
 
-	# initialize a and b value
-	positiveYs = [Y_norm[i] >= 1e-5 for i = 1:N];
-	numPositives = sum(positiveYs);
-
-	A = [ones(numPositives) X[positiveYs]];
-
-	# solves the linear system for initial parameters a and b
-	l, initialParamB = A \ log.(Y_norm[positiveYs]);
 	
-	# a = exp(ln(a))
-	initialParamA = exp(l);
-	
-	
-	print("Initial guess for a: ");
-	println(initialParamA * normVal);
-
-	
-	print("Initial guess for b: ");
-	println(initialParamB)
-
-
 	# tunning the initial parameters
-	paramA = initialParamA;
-	paramB = initialParamB;
+	paramA, paramB = initializeParameters(X, Y_norm);
 
 	loss = [];
+	pointsSeen = 0;
+	
+	JTJ = zeros(2, 2);
+	JTr = zeros(2, 1);
 
 	for iter = 1:maxIter
-		JTJ = zeros(2, 2);
-		JTr = zeros(2, 1);
 		
 		L = 0.0;
 
@@ -49,25 +40,33 @@ function LevenbergMarquardt(X, Y, maxIter = 100, damping = 5)
 
 			ri = paramA * e - Y_norm[i];
 
-			L += ri * ri;
-
 			JTr[1, 1] += ri * e;
 			JTr[2, 1] += ri * paramA * X[i] * e;
-		end
+			L += ri * ri;
 
-    # delayed gratification
-		if (iter != 1 && L > loss[end])
-			damping *= 2;
-		else
-			damping /= 3;
-		end
+			pointsSeen += 1;
 
-		JTJ[1, 1] += damping * JTJ[1, 1];
-		JTJ[2, 2] += damping * JTJ[2, 2];
+			if (pointsSeen == batchSize)
+				
+		    	# delayed gratification
+				if (iter != 1 && L > loss[end])
+					damping *= 2;
+				else
+					damping /= 3;
+				end
 		
-		delta = JTJ \ JTr;
-		paramA -= delta[1];
-		paramB -= delta[2];
+				JTJ[1, 1] += damping * JTJ[1, 1];
+				JTJ[2, 2] += damping * JTJ[2, 2];
+
+				delta = JTJ \ JTr;
+				paramA -= delta[1];
+				paramB -= delta[2];
+			
+				JTJ = zeros(2, 2);
+				JTr = zeros(2, 1);
+				pointsSeen = 0;
+			end
+		end
 
 		push!(loss, L);
 		
