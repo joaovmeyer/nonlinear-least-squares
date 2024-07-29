f8a1e7a61ad78d47847d9089f45a3422e50d0258
Gabriel-Vernizi <155989477+Gabriel-Vernizi@users.noreply.github.com>
2024-07-29 15:32:10 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,303 +1,259 @@
+### 
+using Plots, Random
 ###
-begin
-	using Plots
-	using Random
+function initializeParameters(X, Y)
+	positiveYs = [Y[i] >= 1e-5 for i = 1:length(X)];
+	numPositives = sum(positiveYs);
 
-###
+	A = [ones(numPositives) X[positiveYs]];
 
+	l, initialParamB = A \ log.(Y[positiveYs]);
+
+	return exp(l), initialParamB;
+end
 ###
-function Newton(X, Y, maxIter = 100,initialParameters=true)
+function GaussNewton(X, Y, maxIter = 100, batchSize = -1)
 
 	N = length(X);
+
+	# no batching
+	if (batchSize <= 0)
+		batchSize = N;
+	else
+		# shuffle points if using batches
+		ordem = randperm(N)
+		X = X[ordem]
+		Y = Y[ordem]
+	end
 
 	# normalizing Y value
 	normVal = sum(Y) * 0.1;
 	Y_norm = Y ./ normVal;
 
-	if initialParameters
-		# initialize a and b value
-		positiveYs = [Y_norm[i] >= 1e-5 for i = 1:N];
-		numPositives = sum(positiveYs);
-	
-		A = [ones(numPositives) X[positiveYs]];
-	
-		# solves the linear system for initial parameters a and b
-		l, initialParamB = A \ log.(Y_norm[positiveYs]);
-		
-		# a = epx(ln(a))
-		initialParamA = exp(l);
-		
-		
-		print("Initial guess for a with Newton: ");
-		println(initialParamA * normVal);
-	
-		
-		print("Initial guess for b with Newton: ");
-		println(initialParamB)
-	
-		# tunning the initial parameters
-	
-		paramA = initialParamA;
-		paramB = initialParamB;
-	
-	else
-		
-		paramA,paramB = 0.1, 0.1
-		
-	end
 
+	# tunning the initial parameters
+	paramA, paramB = initializeParameters(X, Y_norm);
 
 	loss = [];
-	
+
+	# points seen since last update
+	pointsSeen = 0;
+
+	# have these outside the training loop because if we are usign batches, the remainder of one batch can be used in the next one.
+	JTJ = zeros(2, 2);
+	JTr = zeros(2, 1);
+
 	for iter = 1:maxIter
-		
-		loss_ri = 0;
-		JTJ = zeros(2, 2);
-		JTr = zeros(2, 1);
+		L = 0.0;
 
 		for i = 1:N
 			e = exp(paramB * X[i]);
 
 			JTJ[1, 1] += e * e;
 			JTJ[1, 2] += paramA * X[i] * e * e;
-			
-			JTJ[2, 1] += paramA * X[i] * e * e;			
+			JTJ[2, 1] += paramA * X[i] * e * e;
 			JTJ[2, 2] += paramA * paramA * X[i] * X[i] * e * e;
 
 			ri = paramA * e - Y_norm[i];
 
 			JTr[1, 1] += ri * e;
 			JTr[2, 1] += ri * paramA * X[i] * e;
-			loss_ri += ri * ri
+			L += ri * ri;
+
+			pointsSeen += 1;
+
+			if (pointsSeen == batchSize)
+				delta = JTJ \ JTr;
+				paramA -= delta[1];
+				paramB -= delta[2];
 			
+				JTJ = zeros(2, 2);
+				JTr = zeros(2, 1);
+				pointsSeen = 0;
+			end
 		end
 
-		append!(loss,loss_ri)
-		
-		delta = JTJ \ JTr;
-		paramA -= delta[1];
-		paramB -= delta[2];
-		
+		push!(loss, L);
 	end
-	print("Final guess for a after $maxIter iterations with Newton: ");
+
+	# maybe at the end some points were seen but not used. It's best to keep
+	# it that way, because if we choose to use them, they have a high chance
+	# of not representing out function well, due to their small number
+
+	
+	print("Final guess for a after $maxIter iterations with Gauss-Newton: ");
 	println(paramA * normVal);
 
 	
-	print("Final guess for b after $maxIter iterations with Newton: ");
+	print("Final guess for b after $maxIter iterations with Gauss-Newton: ");
 	println(paramB)
 	
-	return (paramA * normVal, paramB,loss);
+	return paramA * normVal, paramB, loss;
 end
 ###
-
-###
-function LevenbergMarquardt(X, Y, maxIter = 100,damping=3,initialParameters=true)
+function LevenbergMarquardt(X, Y, maxIter = 100, damping = 5, batchSize = -1)
 
 	N = length(X);
+
+	# no batching
+	if (batchSize <= 0)
+		batchSize = N;
+	else
+		# shuffle points if using batches
+		ordem = randperm(N)
+		X = X[ordem]
+		Y = Y[ordem]
+	end
 
 	# normalizing Y value
 	normVal = sum(Y) * 0.1;
 	Y_norm = Y ./ normVal;
 
-	if initialParameters
-		# initialize a and b value
-		positiveYs = [Y_norm[i] >= 1e-5 for i = 1:N];
-		numPositives = sum(positiveYs);
 	
-		A = [ones(numPositives) X[positiveYs]];
-	
-		# solves the linear system for initial parameters a and b
-		l, initialParamB = A \ log.(Y_norm[positiveYs]);
-		
-		# a = epx(ln(a))
-		initialParamA = exp(l);
-		
-		
-		print("Initial guess for a with LevenbergMarquardt: ");
-		println(initialParamA * normVal);
-	
-		
-		print("Initial guess for b with LevenbergMarquardt: ");
-		println(initialParamB)
-	
-		# tunning the initial parameters
-		paramA = initialParamA;
-		paramB = initialParamB;
-
-	else
-		paramA,paramB = 0.1, 0.1;		
-	end
-
+	# tunning the initial parameters
+	paramA, paramB = initializeParameters(X, Y_norm);
 
 	loss = [];
+	pointsSeen = 0;
 	
+	JTJ = zeros(2, 2);
+	JTr = zeros(2, 1);
+
 	for iter = 1:maxIter
 		
-		loss_ri = 0;
-		JTJ = zeros(2, 2);
-		JTr = zeros(2, 1);
+		L = 0.0;
 
 		for i = 1:N
 			e = exp(paramB * X[i]);
 
 			JTJ[1, 1] += e * e;
 			JTJ[1, 2] += paramA * X[i] * e * e;
-			
-			JTJ[2, 1] += paramA * X[i] * e * e;			
+			JTJ[2, 1] += paramA * X[i] * e * e;
 			JTJ[2, 2] += paramA * paramA * X[i] * X[i] * e * e;
 
 			ri = paramA * e - Y_norm[i];
 
 			JTr[1, 1] += ri * e;
 			JTr[2, 1] += ri * paramA * X[i] * e;
-			loss_ri += ri * ri
-			
-		end
-		append!(loss,loss_ri)
+			L += ri * ri;
+
+			pointsSeen += 1;
+
+			if (pointsSeen == batchSize)
+				
+		    	# delayed gratification
+				if (iter != 1 && L > loss[end])
+					damping *= 2;
+				else
+					damping /= 3;
+				end
 		
-		# delayed gratification
-		if (iter != 1 && loss_ri > loss[end])
-			damping *= 2
-		else
-			damping /= 3
+				JTJ[1, 1] += damping * JTJ[1, 1];
+				JTJ[2, 2] += damping * JTJ[2, 2];
+
+				delta = JTJ \ JTr;
+				paramA -= delta[1];
+				paramB -= delta[2];
+			
+				JTJ = zeros(2, 2);
+				JTr = zeros(2, 1);
+				pointsSeen = 0;
+			end
 		end
 
-		JTJ[1,1] += damping * JTJ[1,1]
-		JTJ[2,2] += damping * JTJ[2,2]
-		
-		
-		delta = JTJ \ JTr;
-		paramA -= delta[1];
-		paramB -= delta[2];
+		push!(loss, L);
 		
 	end
-	print("Final guess for a after $maxIter iterations with LevenbergMarquardt: ");
+	print("Final guess for a after $maxIter iterations with Levenberg Marquardt: ");
 	println(paramA * normVal);
 
 	
-	print("Final guess for b after $maxIter iterations with LevenbergMarquardt: ");
+	print("Final guess for b after $maxIter iterations with Levenberg Marquardt: ");
 	println(paramB)
 	
-	return (paramA * normVal, paramB,loss);
+	return (paramA * normVal, paramB, loss);
 end
 ###
+function GradientDescent(X, Y, maxIter = 500, lr = 0.05, batchSize = -1)
 
-###
-function GradientDescent(X,Y,maxIterations=500,lr=0.05,batchSize=50,initialParameters=true)
-	
-	# normalizing Y value
-	normVal = sum(Y) * 0.1;
-	Y_norm = Y ./ normVal;
-
-	######### initializes a and b parameters #########
 	N = length(X);
-
-	# normalizing Y value
-	normVal = sum(Y) * 0.1;
-	Y_norm = Y ./ normVal;
-
-	if initialParameters
-		# initialize a and b value
-		positiveYs = [Y_norm[i] >= 1e-5 for i = 1:N];
-		numPositives = sum(positiveYs);
+	println(N);
 	
-		A = [ones(numPositives) X[positiveYs]];
-	
-		# solves the linear system for initial parameters a and b
-		l, initialParamB = A \ log.(Y_norm[positiveYs]);
-		
-		# a = exp(ln(a))
-		initialParamA = exp(l);
-		
-		
-		print("Initial guess for a with GradientDescent: ");
-		println(initialParamA * normVal);
-	
-		
-		print("Initial guess for b with GradientDescent: ");
-		println(initialParamB)
-		
-		
-		paramA = initialParamA;
-		paramB = initialParamB;
-	
+	# no batching
+	if (batchSize <= 0)
+		batchSize = N;
 	else
-		paramA,paramB = 0.0, 0.0		
+		# shuffle points if using batches
+		ordem = randperm(N)
+		X = X[ordem]
+		Y = Y[ordem]
 	end
 	
-	##################################################
-	
-		### Training the parameters ###
-	
-	loss = []
-	
-	for iter = 1:maxIterations
-		
-		c = 0.0
-		d = 0.0
-		gradB = 0.0
+	normVal = sum(Y) * 0.1;
+	Y_norm = Y ./ normVal;
 
-		L = 0.0
+
+	# training the parameters
+	paramA, paramB = initializeParameters(X, Y_norm);
+
+	loss = [];
+	
+	pointsSeen = 0;
+	gradA = 0.0;
+	gradB = 0.0;
+
+	for iter = 1:maxIter
+		
+		L = 0.0;
 	
 		for i = 1:N
-			x = X[i]
-			y = Y_norm[i]
+			x = X[i];
+			y = Y_norm[i];
 			
-			e = exp(paramB * x)
+			e = exp(paramB * x);
 
-			# Sum to get the exact value of a based on b
-			# using the partial derivate of dL/da
-			c += e * e
-			d += y * e
+			gradA += (paramA * e - y) * e
+			gradB += (paramA * e - y) * paramA * x * e;
 			
-			
-			gradB += (paramA * e - y) * paramA * x * e
-			L += (paramA * e - y)^2
+			L += (paramA * e - y)^2;
 
+			pointsSeen += 1;
 
-			#update the parameters every {batchSize} iterations
-			if (i % batchSize == 0)		
-				
-				paramA = d / c
-					
-				paramB -= (gradB / batchSize) * lr
-				
-				gradB = 0.0
-				c = 0.0
-				d = 0.0
+			# update the parameters every {batchSize} iterations
+			if (pointsSeen == batchSize)
+				paramA -= (gradA / batchSize) * lr;
+				paramB -= (gradB / batchSize) * lr;
+
+				pointsSeen = 0;
+				gradA = 0.0;
+				gradB = 0.0;
 			end
 
 		end
 
-		append!(loss, L)
-
+		append!(loss, L);
 	end
-	
-	print("Final guess for a after $maxIterations iterations with GradientDescent: ");
+
+	print("Final guess for a after $maxIter iterations with Gradient Descent: ");
 	println(paramA * normVal);
 
 	
-	print("Final guess for b after $maxIterations iterations with GradientDescent: ");
+	print("Final guess for b after $maxIter iterations with Gradient Descent: ");
 	println(paramB)
 	
-	return (paramA * normVal,paramB,loss)
-end	
-###
-
+	return paramA * normVal, paramB, loss;
+	
+end
 ###
 begin 
 
+	Points = 100
+	noise = 10
 	f(x) = π * exp(-.5 * x)
-	noise = 100
+	interval = (-10,10)
 	
-	X = [i for i =-15:0.05:10]
+	X = [i for i = interval[1]:(interval[2]-interval[1])/Points:interval[2]]
 	Y = [f(x) + (rand()-0.5) * noise for x in X]
-	
-	
-	# shuffle our data
-	ordem = randperm(length(X))
-	X = X[ordem]
-	Y = Y[ordem]
 
 	# Plots our data
 	scatter(X, Y)
@@ -305,30 +261,16 @@ begin
  
 end
 ###
-
-###
 begin
-	a,b,loss = Newton(X,Y,100)
+	a,b,loss = GaussNewton(X,Y,100)
 	println()
-	a2,b2,loss2 = GradientDescent(X,Y, 500 ,0.05, 25)
+	a2,b2,loss2 = GradientDescent(X,Y, 999 ,0.005,25)
+	println()
 	a3,b3,loss3 = LevenbergMarquardt(X,Y)
 	
 	scatter(X,Y)
-	plot!(x->a*exp(b*x),lw=2,ls=:dash,color=:purple,label="Newton")
+	plot!(x->a*exp(b*x),lw=2,ls=:dash,color=:purple,label="Gauss-Newton")
 	plot!(x->a2*exp(b2*x),lw=1.5,color=:red,ls=:dash,label="Gradient")
-	plot!(x->a3*exp(b3*x),lw=1.2,label="LevenbergMarquardt")
-end
-###
-
-###
-begin
-	mini = minimum(vec([length(loss) length(loss2) length(loss3)]))
-		
-	interval = [i for i = 1:mini]
-	
-	plot(title="Comparação Erros",ylims=(0,10))
-	plot!(interval,[loss[i] for i = 1:mini],label="Newton")
-	plot!(interval,[loss2[i] for i = 1:mini],label="Gradient")
-	plot!(interval,[loss3[i] for i = 1:mini],label="Levenberg")
+	plot!(x->a3*exp(b3*x),lw=1.2,label="Levenberg Marquardt")
 end
 ###
